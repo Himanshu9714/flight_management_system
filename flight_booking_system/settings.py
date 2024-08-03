16afd34ba45a14944e1ca282ba4f549a7c7fa17c
Himanshu Suthar <h9714348433@gmail.com>
2024-08-03 10:01:06 +0530

    Minor change

--- flight_booking_system/settings.py
+++ flight_booking_system/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "flights",
-    "users"
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = "flight_booking_system.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
